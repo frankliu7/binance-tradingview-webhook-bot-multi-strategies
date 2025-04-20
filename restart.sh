b298e6d105f702a88a2512c6523ae551f9177585
frankliu7 <51848756+frankliu7@users.noreply.github.com>
2025-04-20 20:38:34 +0800

    Update restart.sh

--- restart.sh
+++ restart.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-echo "♻️ 正在重啟 Trading Bot 與 Dashboard..."
-
-./stop.sh
-./stop_dashboard.sh
-
-./start.sh
-./start_dashboard.sh
-
-echo "✅ 已完成重啟！"
+bash stop.sh
+sleep 1
+bash start.sh
